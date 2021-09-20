5e215ca09b3dfb1692ef973a1078b2ad8f04d33c
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-09-20 18:42:59 +0800

    Graphics2D experiment started.

---
+++ com/example/gui/graphic/Graphics2DExperiment.java
@@ -0,0 +1,10 @@
+package graphic;
+
+import java.awt.Graphics2D;
+
+public final class Graphics2DExperiment{
+	public static void main(String[] args) {
+		var graphicsExperiment = new Graphics2DExperiment();
+	}
+}
+
